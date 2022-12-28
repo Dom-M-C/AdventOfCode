c2679e571df02847950b42b98fcbc6da1f41bd91
Dominic Copeland <dominic.copeland@oakbrookfinance.com>
2022-12-28 16:46:21 +0000

    Day one problem one solved with input

--- 2022/app/Main.hs
+++ 2022/app/Main.hs
@@ -1,8 +1,22 @@
 module Main where
 
-import qualified MyLib (someFunc)
+import Paths_Advent2022
+import DayOne.ProblemOne (dayOneProblemOneSolution)
 
 main :: IO ()
-main = do
-  putStrLn "Hello, Haskell!"
-  MyLib.someFunc
+main = dayOneProblemOne 
+--  >> testMain
+
+testMain = do 
+  testTxt <- readDataFile "data/test.txt"
+  putStrLn testTxt
+
+
+dayOneProblemOne = do
+  input <- readDataFile "data/day_one.txt"
+  let maxCalories = dayOneProblemOneSolution input
+  putStrLn $ "day one, problem one: " <> maxCalories
+
+
+readDataFile fileName = getDataFileName fileName >>= readFile 
+
--- 2022/src/DayOne/ProblemOne.hs
+++ 2022/src/DayOne/ProblemOne.hs
@@ -1,28 +1,13 @@
-module DayOne.ProblemOne where
-
-dayOneProblemOneTestInput = unlines 
-  [   "1000"
-  ,   "2000"
-  ,   "3000"
-  ,   ""
-  ,   "4000"
-  ,   ""
-  ,   "5000"
-  ,   "6000"
-  ,   ""
-  ,   "7000"
-  ,   "8000"
-  ,   "9000"
-  ,   ""
-  ,   "10000"
-  ]
+module DayOne.ProblemOne (getElvesCalories, dayOneProblemOneSolution) where
 
 getElvesCalories :: String -> [Int]
-getElvesCalories = map elfCalories. foldr foldElf [Elf 0] . lines
+getElvesCalories = map elfCalories . foldr foldElf [Elf 0] . lines
   where
     foldElf :: String -> [Elf] -> [Elf]
     foldElf [] elves = Elf 0:elves
     foldElf calories (elf:elves) = Elf (read calories + elfCalories elf) : elves
 
+dayOneProblemOneSolution :: String -> String
+dayOneProblemOneSolution = show . foldr max 0 . getElvesCalories
 
 newtype Elf = Elf { elfCalories :: Int}
