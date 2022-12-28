093af8f15062a9690a84ae021832acef789c7421
Dominic Copeland <dominic.copeland@oakbrookfinance.com>
2022-12-28 16:54:11 +0000

    Add type signatures

--- 2022/app/Main.hs
+++ 2022/app/Main.hs
@@ -7,16 +7,17 @@ main :: IO ()
 main = dayOneProblemOne 
 --  >> testMain
 
+testMain :: IO ()
 testMain = do 
   testTxt <- readDataFile "data/test.txt"
   putStrLn testTxt
 
-
+dayOneProblemOne :: IO ()
 dayOneProblemOne = do
   input <- readDataFile "data/day_one.txt"
   let maxCalories = dayOneProblemOneSolution input
   putStrLn $ "day one, problem one: " <> maxCalories
 
-
+readDataFile :: FilePath -> IO String
 readDataFile fileName = getDataFileName fileName >>= readFile 
 
